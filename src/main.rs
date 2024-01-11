713696d11593c69648c894bd026fc27865cc430a
Puneet56 <puneetdhiman56@gmail.com>
2024-01-12 00:54:03 +0530

    feat(repl): Repl to tokenize the input and print the tokens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 pub mod lexer;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to AtomScript!");
+    println!("Feel free to type in commands...");
+
+    let mut input = String::new();
+    loop {
+        std::io::stdin().read_line(&mut input).unwrap();
+        let mut lexer = lexer::Lexer::new(input.clone());
+        loop {
+            let token = lexer.next_token();
+            println!("{:?}", token);
+            if token == lexer::Token::Eof {
+                break;
+            }
+        }
+        input.clear();
+    }
 }
